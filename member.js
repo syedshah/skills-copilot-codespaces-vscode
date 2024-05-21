59ef5760459df1f41994ef15feb95836eae17a45
Syed Shah <Syed.Shah@lancashiregroup.com>
2024-05-21 12:21:26 +0100

    Copilot update the skillsMember function

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'MongoDB',
+    'Python',
+    'Java',
+    'C++',
+    'C#',
+  ];
+  return skills;
+}
